0117f12b71568ef08cc234a617de3674707ff641
Artur Zayats <zag2art@gmail.com>
2014-04-09 18:24:23 +0400

    user tag

--- test/parse.js
+++ test/parse.js
@@ -6,17 +6,17 @@ describe('bcrypt', function () {
   describe('#parse', function () {
     it('should parse [b] to <b>', function () {
       var parse = bbcode.parse('[b]Bold[/b]');
-      parse.should.equal('<b>Bold</b>');
+      parse.html.should.equal('<b>Bold</b>');
     });
 
     it('should parse [i] to <i>', function () {
       var parse = bbcode.parse('[i]italics[/i]');
-      parse.should.equal('<i>italics</i>');
+      parse.html.should.equal('<i>italics</i>');
     });
 
     it('should parse [u] to <u>', function () {
       var parse = bbcode.parse('[u]underline[/u]');
-      parse.should.equal('<u>underline</u>');
+      parse.html.should.equal('<u>underline</u>');
     });
 
 
@@ -26,32 +26,32 @@ describe('bcrypt', function () {
       for (var i = 0; i < colors.length; i++) {
         var color = colors[i];
         var parse = bbcode.parse('[color=' + color + ']' + color + '[/color]');
-        parse.should.equal('<span style="color: ' + color + '">' + color + '</span>');
+        parse.html.should.equal('<span style="color: ' + color + '">' + color + '</span>');
       }
     });
 
 
     it('should parse [s] to <span style="text-decoration: line-through"', function () {
       var parse = bbcode.parse('[s]strikethrough[/s]');
-      parse.should.equal('<s>strikethrough</s>');
+      parse.html.should.equal('<s>strikethrough</s>');
     });
 
     describe('should parse [url]', function () {
       it('should parse [url=<url>] to <a href=<url>>', function () {
         var parse = bbcode.parse('[url=http://example.com]url[/url]');
-        parse.should.equal('<a target="_blank" href="http://example.com">url</a>');
+        parse.html.should.equal('<a target="_blank" href="http://example.com">url</a>');
 
       });
 
       it('should parse [url="<url>"] to <a href=<url>>', function () {
         var parse = bbcode.parse('[url="http://example.com"]url[/url]');
-        parse.should.equal('<a target="_blank" href="http://example.com">url</a>');
+        parse.html.should.equal('<a target="_blank" href="http://example.com">url</a>');
 
       });
 
       it('should parse [url=\'<url>\'] to <a href=<url>>', function () {
         var parse = bbcode.parse('[url=\'http://example.com\']url[/url]');
-        parse.should.equal('<a target="_blank" href="http://example.com">url</a>');
+        parse.html.should.equal('<a target="_blank" href="http://example.com">url</a>');
 
       });
 
@@ -60,63 +60,70 @@ describe('bcrypt', function () {
     describe('should parse [img]', function () {
       it('as attribute - [img=<img>] to <img src=<img>>', function () {
         var parse = bbcode.parse('[img=http://example.com/img.png][/img]');
-        parse.should.equal('<img src="http://example.com/img.png" />');
+        parse.html.should.equal('<img src="http://example.com/img.png" />');
       });
 
       it('as attribute with quotes - [img="<img>"] to <img src=<img>>', function () {
         var parse = bbcode.parse('[img="http://example.com/img.png"][/img]');
-        parse.should.equal('<img src="http://example.com/img.png" />');
+        parse.html.should.equal('<img src="http://example.com/img.png" />');
       });
 
       it('as attribute with single quotes - [img=\'<img>\'] to <img src=<img>>', function () {
         var parse = bbcode.parse('[img=\'http://example.com/img.png\'][/img]');
-        parse.should.equal('<img src="http://example.com/img.png" />');
+        parse.html.should.equal('<img src="http://example.com/img.png" />');
       });
 
       it('as content - [img]<img>[/img] to <img src=<img>>', function () {
         var parse = bbcode.parse('[img]http://example.com/img.png[/img]');
-        parse.should.equal('<img src="http://example.com/img.png" />');
+        parse.html.should.equal('<img src="http://example.com/img.png" />');
       });
     });
 
     describe('should parse [skin]', function () {
-      it('as attribute - [skin]name[/skin] to <img src="name">', function () {
+      it('as content - [skin]name[/skin] to <img src="name">', function () {
         var parse = bbcode.parse('[skin]name[/skin]');
-        parse.should.equal('<img src=\"http://bombermine.com/api/ctor/getskin?name=name&thumb=1\" />');
+        parse.html.should.equal('<img class="bb-skin" src="http://bombermine.com/api/ctor/getskin?name=name&thumb=1" />');
+      });
+    });
+
+    describe('should parse @nickName', function () {
+      it('@nickname to <user name="{uid}"/>', function () {
+        var parse = bbcode.parse('@nickname');
+        (!!parse.users['nickname']).should.equal(true);
       });
     });
 
     it('should parse [quote] as <blockquote>', function () {
       var parse = bbcode.parse('[quote=person]quoted[/quote]');
-      parse.should.equal('<blockquote cite="person">quoted</blockquote>');
+      parse.html.should.equal('<blockquote cite="person">quoted</blockquote>');
     });
 
     it('should try to fix broken markup', function () {
       var parse = bbcode.parse('[b]test');
-      parse.should.equal('<b>test</b>');
+      parse.html.should.equal('<b>test</b>');
     });
 
 
     it('should parse inner tags [b][i][u] to <b><i><u>', function () {
       var parse = bbcode.parse('[b][i][u]Hai[/u][/i][/b]');
-      parse.should.equal('<b><i><u>Hai</u></i></b>');
+      parse.html.should.equal('<b><i><u>Hai</u></i></b>');
 
     });
 
     describe('should parse uppercase tags properly - ', function () {
       it('should parse uppercase [I] to <i>', function () {
         var parse = bbcode.parse('[I]italics[/I]');
-        parse.should.equal('<i>italics</i>');
+        parse.html.should.equal('<i>italics</i>');
 
       });
       it('should parse uppercase [IMG] to <img>', function () {
         var parse = bbcode.parse('[IMG]http://example.com/img.png[/IMG]');
-        parse.should.equal('<img src="http://example.com/img.png" />');
+        parse.html.should.equal('<img src="http://example.com/img.png" />');
 
       });
       it('should parse mixed case [IMG][/img] to <img>', function () {
         var parse = bbcode.parse('[Img]http://example.com/img.png[/img]');
-        parse.should.equal('<img src="http://example.com/img.png" />');
+        parse.html.should.equal('<img src="http://example.com/img.png" />');
       });
     });
   });
